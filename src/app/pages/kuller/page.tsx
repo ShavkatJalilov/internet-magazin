f5093bd456729c09d92c2a3ec838943d990d4418
BORETS2002 <BORETS2002@example.com>
2023-09-09 11:33:06 +0500

    test

--- src/app/pages/kuller/page.tsx
+++ src/app/pages/kuller/page.tsx
@@ -5,7 +5,11 @@ const page = () => {
   return (
     <>
       <div className='container'>
-        <div className=' kuller_img  flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row   mt-5 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4'>
+        <h2 className='text-center text-4xl font-medium title-font text-blue-900  pb-4 mt-5'>
+          Куллер
+        </h2>
+
+        <div className=' ml-16 kuller_img  flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row   mt-5 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4'>
           <Image
             className='img object-cover    rounded  md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
             src='/img/kullerBir.jpg'
@@ -16,7 +20,7 @@ const page = () => {
           />
 
           <div className='box_img flex ml-4 flex-col justify-between  leading-normal'>
-            <span className='flex'>
+            <span className='lg:flex'>
               <span className=' '>
                 {" "}
                 <h5 className='mb-1 text-xl font-bold   text-gray-900 dark:text-white'>
@@ -53,7 +57,123 @@ const page = () => {
             <Link
               target='_blank'
               href='https://t.me/duawater_bot'
-              className='text-indigo-500 mt-2 text-lg '
+              className='text-indigo-500 mt-2 text-lg  md-w-1/2  '
+            >
+              <h2 className='   p-5 rounded-full  bg-blue-400 text-white  text-center text-lg'>
+                заказать
+              </h2>
+            </Link>
+          </div>
+        </div>
+
+        <div className='ml-auto  mr-16 kuller_img  flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row   mt-5 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4'>
+          <div className='box_img flex ml-4 flex-col justify-between  leading-normal'>
+            <span className='lg:flex'>
+              <span className=' '>
+                {" "}
+                <h5 className='mb-1 text-xl font-bold   text-gray-900 dark:text-white'>
+                  Куллер чай от ZIFFLER
+                </h5>
+                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+                  Чай bar Электроник холодилник Сенсорний управление 2 типа{" "}
+                  чайника <br /> Удобно наливать воду из нижней части
+                  Температура нагревания 40 С до100 C Температура охлаждения 6 С
+                  до10 C Доставка по городу бесплатно.
+                </p>
+                <mark className='btn text-xl p-1  rounded-md text-red-600 mt-1'>
+                  Подарок 5 капсул воды
+                </mark>
+                <span className='text-2xl block mt-2'>2 250 000 сум</span>
+              </span>
+
+              <span className='  ml-2  '>
+                {" "}
+                <h5 className='mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                  Куллер чай от ZIFFLER
+                </h5>
+                <p className=' mb-2 font-normal text-gray-700 dark:text-gray-400'>
+                  Аренда только для юридических лиц Каждое 3 месяц должно купит
+                  200 капсул воды Доставка бесплатно
+                </p>
+                <mark className=' text-xl p-1  rounded-md text-red-600  '>
+                  Куллер бесплатно
+                </mark>
+                <span className='text-xl block mt-1 '>
+                  3 000 000 на 3 месяца
+                </span>
+              </span>
+            </span>
+            <Link
+              target='_blank'
+              href='https://t.me/duawater_bot'
+              className='text-indigo-500 mt-2 text-lg  md-w-1/2  '
+            >
+              <h2 className='   p-5 rounded-full  bg-blue-400 text-white  text-center text-lg'>
+                заказать
+              </h2>
+            </Link>
+          </div>
+
+          <Image
+            className='img object-cover    rounded  md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
+            src='/img/kuller3.jpg'
+            alt='foto logo'
+            // style={{ width: "auto", height: "auto" }}
+            width={310}
+            height={410}
+          />
+        </div>
+
+        <div className=' ml-16 kuller_img  flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row   mt-5 hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700 p-4'>
+          <Image
+            className='img object-cover    rounded  md:h-auto md:w-48 md:rounded-none md:rounded-l-lg'
+            src='/img/kuller2.jpg'
+            alt='foto logo'
+            // style={{ width: "auto", height: "auto" }}
+            width={310}
+            height={410}
+          />
+
+          <div className='box_img flex ml-4 flex-col justify-between  leading-normal'>
+            <span className='lg:flex'>
+              <span className=' max-w-xs'>
+                {" "}
+                <h5 className='mb-1 text-xl font-bold   text-gray-900 dark:text-white'>
+                  IWD-1134B
+                </h5>
+                <p className='mb-3 font-normal text-gray-700 dark:text-gray-400'>
+                  Куллер от ZIFFLER Цвет корпуса - чёрный Материал - Металл,
+                  пластик Количество кранов для подачи воды - 3шт Охлаждение -
+                  компрессорное Температура нагревания 40 С до 90 C Температура
+                  охлаждения 6 С до10 C Доставка по городу бесплатно.
+                </p>
+                <mark className='btn text-xl p-1  rounded-md text-red-600 mt-1'>
+                  Подарок 5 капсул воды
+                </mark>
+                <span className='text-2xl block mt-2'>1 500 000 сум</span>
+              </span>
+
+              <span className='  ml-2  '>
+                {" "}
+                <h5 className='mb-1 text-xl font-bold tracking-tight text-gray-900 dark:text-white'>
+                  IWD-1134B
+                </h5>
+                <p className=' mb-2 font-normal text-gray-700 dark:text-gray-400'>
+                  Аренда только для юридических лиц Каждое 3 месяц должно купит
+                  150 капсул воды Доставка бесплатно
+                </p>
+                <mark className=' text-xl p-1  rounded-md text-red-600  '>
+                  Куллер бесплатно
+                </mark>
+                <span className='text-xl block mt-1 '>
+                  2 250 000 на 3 месяца
+                </span>
+              </span>
+            </span>
+            <Link
+              target='_blank'
+              href='https://t.me/duawater_bot'
+              className='text-indigo-500 mt-2 text-lg  md-w-1/2  '
             >
               <h2 className='   p-5 rounded-full  bg-blue-400 text-white  text-center text-lg'>
                 заказать
