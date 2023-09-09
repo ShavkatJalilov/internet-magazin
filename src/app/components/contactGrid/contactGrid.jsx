bc423a7e1da84c5744014849f51da9f8c51deb1f
BORETS2002 <BORETS2002@example.com>
2023-09-09 11:53:24 +0500

    test

--- src/app/components/contactGrid/contactGrid.jsx
+++ src/app/components/contactGrid/contactGrid.jsx
@@ -96,8 +96,8 @@ const ContactGrid = () => {
                 width={400}
                 height={350}
                 className='h-full max-w-full rounded-xl drop-shadow-xl transition duration-300 ease-in-out hover:scale-90'
-                src='/img/stakan1.jpg '
-                alt='foto'
+                src='/img/chashka.jpg'
+                alt=''
               />
             </div>
           </div>
